aa85b7d0e6cec991124e7f70d6d96b03f74449f5
Bob Bass <bob@bbass.co>
2021-05-05 15:58:27 -0400

    added all files

---
+++ README.md
@@ -0,0 +1,26 @@
+# Imperative vs Declarative Programming
+
+## This repo goes with the YouTube video titled:
+
+> ▶️ [Struggling to Learn Web Dev? Say yes to declarative code 🚀 Escape tutorial HELL 🚀](https://youtu.be/uh3WE5BaBT4)<br />
+> by [DataBass Software on YouTube](youtube.com/databasssoftware)
+
+### Purpose
+
+- This video is to share the idea that learning a framework and using declarative code as opposed to vanilla JS and imperative code is exactly what beginners who are stuck and frustrated.
+- That sentiment was originally popularized by Maximilian Schwarzmüller or Academind, the best (in my opinion) web development teacher on YouTube and Udemy.
+
+### File Notes
+
+#### 📁 imperative
+
+- This code is based on the [How TO Create a To Do List - W3 Schools](https://www.w3schools.com/howto/howto_js_todolist.asp)
+
+#### 📁 declarative
+
+- The `index.html` file will run with `Live Server` because it uses the VUE CDN
+- The `index.vue` file needs to be compiled and is only provided for reference
+
+### Notes
+
+The example provided was in Vue.js. Vue is only 1 of many declarative frameworks in the JS ecosystem.
---
+++ declarative/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <section>
+    <h2>Declarative App</h2>
+    <input type="text" v-model="taskInput" />
+    <a @click="addNewTask" style="cursor: pointer">✅</a>
+    <ul>
+      <li
+        v-for="(task, i) in tasks"
+        :key="i"
+        :class="task.completed ? 'item-style' : 'item-style__checked'"
+      >
+        <span>
+          <strong @click="task.completed = !task.completed">
+            {{ task.taskName }}
+          </strong>
+          <a @click="removeTask(i)">❌</a>
+        </span>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style>
+  * {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 20px;
+  }
+  li {
+    list-style: none;
+  }
+  .item-style {
+    text-decoration: line-through;
+  }
+  .item-style__checked {
+    text-decoration: none;
+  }
+</style>
+
+<script>
+  const appData = {
+    data() {
+      return {
+        taskInput: '',
+        tasks: [],
+      };
+    },
+    methods: {
+      addNewTask() {
+        if (!this.taskInput) {
+          alert('please add a task');
+        } else {
+          let newTask = {
+            taskName: this.taskInput,
+            completed: false,
+          };
+          this.tasks.push(newTask);
+          this.taskInput = '';
+        }
+      },
+      removeTask(i) {
+        this.tasks.splice(i, 1);
+      },
+    },
+  };
+</script>
